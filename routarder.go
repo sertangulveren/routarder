c4e1e7e6c33386798dfe56594b5753c6ce5c9e74
Sertan Gülveren <sertangulveren@gmail.com>
2022-01-08 04:43:47 +0300

    grouping and handling

--- routarder.go
+++ routarder.go
@@ -1,5 +1,12 @@
 package routarder
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+)
+
 type Router struct {
 	prefix   string
 	routes   []string
@@ -15,4 +22,32 @@ func Serve(addr string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func (router *Router) Group(pattern string) *Router {
+	r := New()
+	r.prefix = path.Join(router.prefix, pattern)
+	router.children = append(router.children, r)
+	return r
+}
+
+func (router *Router) RouteTree() string {
+	res := router.selfRoutes()
+	for _, c := range router.children {
+		res += c.RouteTree()
+	}
+	return res
+}
+
+func (router *Router) selfRoutes() string {
+	res := ""
+	for _, route := range router.routes {
+		res = fmt.Sprintf("%s%s\n", res, path.Join(router.prefix, route))
+	}
+	return res
+}
+
+func (router *Router) HandleFunc(pattern string, handler http.HandlerFunc) {
+	router.routes = append(router.routes, pattern)
+	http.HandleFunc(path.Join(router.prefix, pattern), handler)
 }
